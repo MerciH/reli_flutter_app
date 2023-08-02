e7ada67706898d5e188e07f2edab5c85addd26a8
Merci <habmercima@gmail.com>
2023-08-03 01:54:14 +0900

    gallery category page

---
+++ lib/gallery.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(GalleryPage());
+
+class GalleryPage extends StatefulWidget{
+  // const GalleryPage(Key? Key): super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _GalleryPage()
+  }
+  
+  class _GalleryPage extends State<GalleryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: ,)
+    )
+  }
+  }
